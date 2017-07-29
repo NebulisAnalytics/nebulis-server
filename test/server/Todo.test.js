e382d7de72d49168dd92a70d3c652779195eedf1
Christopher Dobler <chrisdobler@gmail.com>
2017-07-28 19:39:14 -0700

    updates to sample tests

--- test/server/Todo.test.js
+++ test/server/Todo.test.js
@@ -4,53 +4,53 @@ var should = chai.should();
 chai.use(chaiHttp);
 var expect = chai.expect;
 
-describe('.getAll()', function() {
-	it('should return array of todos', function(done) {
-		Todo.getAll(function(err, todos) {
-			expect(todos).to.be.an('array');
-			done();
-		});
-	});
+xdescribe('.getAll()', function() {
+  it('should return array of todos', function(done) {
+    Todo.getAll(function(err, todos) {
+      expect(todos).to.be.an('array');
+      done();
+    });
+  });
 });
 
-describe('.createTodo()', function() {
-	it('creates a todo', function(done) {
-		Todo.createTodo(null, function(err, todo) {
-			expect(todo).to.be.an('object');
-			expect(todo.name).to.be.equal('null');
-			expect(todo.completed).to.be.equal(false);
-			Todo.getAll(function(err, todos) {
-				expect(todos.length).to.be.above(0);
-				done();
-			});
-		});
-	});
+xdescribe('.createTodo()', function() {
+  it('creates a todo', function(done) {
+    Todo.createTodo(null, function(err, todo) {
+      expect(todo).to.be.an('object');
+      expect(todo.name).to.be.equal('null');
+      expect(todo.completed).to.be.equal(false);
+      Todo.getAll(function(err, todos) {
+        expect(todos.length).to.be.above(0);
+        done();
+      });
+    });
+  });
 });
 
-describe('.deleteTodo()', function() {
-	it('deletes a todo', function(done) {
-		Todo.createTodo('test', function(err, results) {
-			var id = results.id;
-			Todo.deleteTodo(id, function(err, results) {
-				expect(results).to.have.length(1);
-				Todo.findOne({id:id}, function(err, todo) {
-					expect(todo).to.be.equal(undefined);
-					done();
-				});
-			});
-		})
-	});
+xdescribe('.deleteTodo()', function() {
+  it('deletes a todo', function(done) {
+    Todo.createTodo('test', function(err, results) {
+      var id = results.id;
+      Todo.deleteTodo(id, function(err, results) {
+        expect(results).to.have.length(1);
+        Todo.findOne({id:id}, function(err, todo) {
+          expect(todo).to.be.equal(undefined);
+          done();
+        });
+      });
+    })
+  });
 });
 
-describe('.deleteAll()', function() {
-	it('deletes all todos', function(done) {
-		Todo.createTodo(null, function(err, todo) {
-			Todo.deleteAll(function(err, results) {
-				Todo.getAll(function(err, todos) {
-					expect(todos.length).to.be.equal(0);
-					done();
-				});
-			});
-		});
-	});
+xdescribe('.deleteAll()', function() {
+  it('deletes all todos', function(done) {
+    Todo.createTodo(null, function(err, todo) {
+      Todo.deleteAll(function(err, results) {
+        Todo.getAll(function(err, todos) {
+          expect(todos.length).to.be.equal(0);
+          done();
+        });
+      });
+    });
+  });
 });
--- test/server/TodosController.test.js
+++ test/server/TodosController.test.js
@@ -7,77 +7,78 @@ var expect = chai.expect;
 
 var server = 'http://localhost:1337';
 
-describe('GET /api/todos', function() {
-	it('should return array of todos', function(done) {
-		chai.request(server)
-			.get('/api/todos')
-			.end(function(err, res){
-				res.should.have.status(200);
-				res.should.be.json;
-				res.body.should.be.an('array');
-				console.log('/todos', res.body);
-				done();
-			});
-	});
+
+
+xdescribe('GET /api/todos', function() {
+  it('should return array of todos', function(done) {
+    chai.request(server)
+      .get('/api/todos')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.an('array');
+        console.log('/todos', res.body);
+        done();
+      });
+  });
 });
 
-describe('POST /api/todos', function() {
-	it('creates a todo', function(done) {
-		chai.request(server)
-			.post('/api/todos')
-			.send({
-				name: 'test'
-			})
-			.end(function(err, res){
-				expect(res.body.name).to.be.equal('test');
-				expect(res.body.completed).to.be.equal(false);
-				done();
-			});
-	});
+xdescribe('POST /api/todos', function() {
+  it('creates a todo', function(done) {
+    chai.request(server)
+      .post('/api/todos')
+      .send({
+        name: 'test'
+      })
+      .end(function(err, res){
+        expect(res.body.name).to.be.equal('test');
+        expect(res.body.completed).to.be.equal(false);
+        done();
+      });
+  });
 });
 
-describe('PUT /api/todos/:id', function() {
-	it('updates a todo', function(done) {
-		
-		chai.request(server)
-		.post('/api/todos')
-		.send({
-			name: 'test',
-			completed: false
-		})
-		.end(function(err, res){
-			
-			var todo = res.body;
-			var id = todo.id;
-			expect(todo.completed).to.be.equal(false);
-			
-			chai.request(server)
-			.put('/api/todos/'+id).send({
-				completed: true
-			})
-			.end(function(err, res){
-				chai.request(server)
-				.get('/api/todos/'+id)
-				.end(function(err, res){
-					expect(res.body.completed).to.be.equal(true);
-					done();
-				});
-			});
-		});
-	});
+xdescribe('PUT /api/todos/:id', function() {
+  it('updates a todo', function(done) {
+    chai.request(server)
+      .post('/api/todos')
+      .send({
+        name: 'test',
+        completed: false
+      })
+      .end(function(err, res){
+        
+        var todo = res.body;
+        var id = todo.id;
+        expect(todo.completed).to.be.equal(false);
+        
+        chai.request(server)
+          .put('/api/todos/'+id).send({
+            completed: true
+          })
+          .end(function(err, res){
+            chai.request(server)
+              .get('/api/todos/'+id)
+              .end(function(err, res){
+                expect(res.body.completed).to.be.equal(true);
+                done();
+              });
+          });
+      });
+  });
 });
 
-describe('DELETE /api/todos', function() {
-	it('deletes all todos', function(done) {
-		chai.request(server)
-			.del('/api/todos')
-			.end(function(err, res){
-				chai.request(server)
-					.get('/api/todos')
-					.end(function(err, res){
-						expect(res.body.length).to.be.equal(0);
-						done();
-					});
-			});
-	});
+xdescribe('DELETE /api/todos', function() {
+  it('deletes all todos', function(done) {
+    chai.request(server)
+      .del('/api/todos')
+      .end(function(err, res){
+        chai.request(server)
+          .get('/api/todos')
+          .end(function(err, res){
+            expect(res.body.length).to.be.equal(0);
+            done();
+          });
+      });
+  });
 });
