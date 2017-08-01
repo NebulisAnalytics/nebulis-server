4ebf26e04ce24246475d063bb6f33ebcb692440e
Christopher Dobler <chrisdobler@gmail.com>
2017-08-01 01:02:17 -0700

    Adds Endpoint Controller

---
+++ api/controllers/EndpointController.js
@@ -0,0 +1,11 @@
+/**
+ * EndpointController
+ *
+ * @description :: Server-side logic for managing endpoints
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
