bed85821f3ca46ad330a30be92b09cfa4d880a8d
Christopher Dobler <chrisdobler@gmail.com>
2017-08-01 12:39:47 -0700

    Adds Team API

---
+++ api/controllers/TeamController.js
@@ -0,0 +1,11 @@
+/**
+ * TeamController
+ *
+ * @description :: Server-side logic for managing teams
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
