28d21ea12231c29622728323da95fed63401f50f
Christopher Dobler <chrisdobler@gmail.com>
2017-08-01 16:43:11 -0700

    Adds member test for endpoint relationship

--- test/server/Member.test.js
+++ test/server/Member.test.js
@@ -4,21 +4,28 @@ var should = chai.should();
 chai.use(chaiHttp);
 var expect = chai.expect;
 
-xdescribe('Relationships', function() {
-  before(function(done) {
-    Member.create(null).exec(function(member) {
-      member.endpoints.add({});
-      member.teams.add({});
-      member.save(function(err) {
-        done();
+describe('Relationships', function() {
+  before(async function() {
+    const member = await Member.create({username: 'trustyPartner'});
+    const endpoint = await Endpoint.create({member: member.id});
+    const team = Team.create({member: member.id});
+  });
+  after(function(done) {
+    Member.find({username: 'trustyPartner'})
+      .populate('endpoints')
+      .exec(function(err, members) {
+        Endpoint.destroy({member: members[0].id}).exec();
+        Member.destroy({id: members[0]}).exec(function(err, member) {});
       });
-    });
+    Member.destroy({username: 'trustyPartner'}).exec(function(err, member) {});
+    done();
   });
   it('should be able to get a list of it\'s endpoints', function(done) {
-    Member.find()
+    Member.find({username: 'trustyPartner'})
       .populate('endpoints')
       .exec(function(err, members) {
         expect(members[0].endpoints.length).to.be.equal(1);
+        done();
     });
   });
   xit('should be able to list it\'s team memberships', function(done) {
