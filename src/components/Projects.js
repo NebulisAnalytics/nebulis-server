0e155186b710097b8af9060d54352a9cb87e36e8
ksher <ksher5@gmail.com>
2017-08-03 12:47:36 -0700

    added updatedAt to display in project list

---
+++ src/components/Projects.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+
+export default class Projects extends Component {
+	render() {
+		return (
+			<div className="projects">
+				{ this.renderProjects() }
+			</div>);
+	}
+
+	renderProjects() {
+		if (this.props.projects.length) {
+			return (<ul>
+				{ this.props.projects.map((project, index) => {
+					let completeHandler = () => {
+						this.props.onToggleCompleted(project.id, !project.completed);
+					};
+					let deleteHandler = () => {
+						this.props.onDelete(project.id);
+					};
+					return (<Project key={index} project={project} onDownload={completeHandler} onDelete={deleteHandler}/>);
+				}) }
+			</ul>);
+		}
+	}
+}
+
+class Project extends Component {
+	render() {
+		const project = this.props.project;
+		return (
+			<li className="project">
+				<Link to="./../project:id"><span>
+					{ project.name }
+					{ project.updatedAt }
+				</span></Link>
+				&nbsp;
+				<a href="javascript://" onClick={ ::this.props.onDownload }>
+					✓
+				</a>
+				&nbsp;
+				<a href="javascript://" onClick={ ::this.props.onDelete }>
+					×
+				</a>
+			</li>);
+	}
+}
