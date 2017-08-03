a9127117f60aef71c026061788d4e0c35091f021
ksher <ksher5@gmail.com>
2017-08-03 12:41:47 -0700

    added in nebulis actions reference

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,51 +2,83 @@ import {createStore, compose, applyMiddleware} from 'redux';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+import es6promise from 'es6-promise';
+es6promise.polyfill();
 
-function configureStoreProd(initialState) {
-  const middlewares = [
-    // Add other middleware on this line...
+// import the generated reducer and actions
 
-    // thunk middleware can also accept an extra argument to be passed to each thunk action
-    // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
-    thunk,
-  ];
+import {actions as todosActions, addProject, closeProject} from './../actions/nebulisActions';
 
-  return createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middlewares)
-    )
-  );
+// export the actions so they can be used elsewhere in the app
+export {todosActions, addProject, closeProject};
+
+
+
+// create the redux store
+export const store = createStore(
+	rootReducer,
+	applyMiddleware(thunk)
+);
+
+export function getStore() {
+	return store;
 }
 
-function configureStoreDev(initialState) {
-  const middlewares = [
-    // Add other middleware on this line...
+// window.getStore = getStore;
 
-    // Redux middleware that spits an error on you when you try to mutate your state either inside a dispatch or between dispatches.
-    reduxImmutableStateInvariant(),
 
-    // thunk middleware can also accept an extra argument to be passed to each thunk action
-    // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
-    thunk,
-  ];
-
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
-  const store = createStore(rootReducer, initialState, composeEnhancers(
-    applyMiddleware(...middlewares)
-    )
-  );
-
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
-      const nextReducer = require('../reducers').default; // eslint-disable-line global-require
-      store.replaceReducer(nextReducer);
-    });
-  }
-
-  return store;
-}
-
-const configureStore = process.env.NODE_ENV === 'production' ? configureStoreProd : configureStoreDev;
-
-export default configureStore;
+//
+// function getStoreProd(initialState) {
+//   const middlewares = [
+//     // Add other middleware on this line...
+//
+//     // thunk middleware can also accept an extra argument to be passed to each thunk action
+//     // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
+//     thunk,
+//   ];
+//
+//   return createStore(rootReducer, initialState, compose(
+//     applyMiddleware(...middlewares)
+//     )
+//   );
+// }
+//
+// function getStoreDev(initialState) {
+//   const middlewares = [
+//     // Add other middleware on this line...
+//
+//     // Redux middleware that spits an error on you when you try to mutate your state either inside a dispatch or between dispatches.
+//     reduxImmutableStateInvariant(),
+//
+//     // thunk middleware can also accept an extra argument to be passed to each thunk action
+//     // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
+//     thunk,
+//   ];
+//
+//   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
+//   const store = createStore(rootReducer, initialState, composeEnhancers(
+//     applyMiddleware(...middlewares)
+//     )
+//   );
+//
+//   if (module.hot) {
+//     // Enable Webpack hot module replacement for reducers
+//     module.hot.accept('../reducers', () => {
+//       const nextReducer = require('../reducers').default; // eslint-disable-line global-require
+//       store.replaceReducer(nextReducer);
+//     });
+//   }
+//
+//   return store;
+// }
+//
+// const getStore = process.env.NODE_ENV === 'production' ? getStoreProd : getStoreDev;
+//
+//
+//
+//
+//
+//
+//
+//
+// export default getStore;
