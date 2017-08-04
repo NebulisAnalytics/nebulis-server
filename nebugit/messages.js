72788cf70be6f7b5441d2f377449241a121fa30e
Christopher Dobler <chrisdobler@gmail.com>
2017-08-04 15:03:16 -0700

    Adds git messages

--- nebugit/messages.js
+++ nebugit/messages.js
@@ -21,10 +21,12 @@ const logoText = `                                                     .,,,,
      ,:;;;;;;;;;:,.\n`;
 
 const connectionText = `Nebulis git subsystem is listening on %s:%s`.magenta;
+const listenerConnectionText = 'Nebulis Listener is listening on http://%s:%s'.magenta;
 
 const messages = {
   logo: () => { console.log(logoText, '0.1.0'); },
-  connectionInfo: (ip, port) => { console.log(connectionText, ip, port)}
+  connectionInfo: (ip, port) => { console.log(connectionText, ip, port)},
+  listenerConnectionInfo: (host, port) => {console.log(listenerConnectionText, host, port)}
 };
 
 export { messages as default };
