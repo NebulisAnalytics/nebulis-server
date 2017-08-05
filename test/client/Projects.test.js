3692ee062ede89aea4e194838dc290d7ae69af28
ksher <ksher5@gmail.com>
2017-08-05 14:44:58 -0700

    projects files

--- test/client/Projects.test.js
+++ test/client/Projects.test.js
@@ -10,6 +10,11 @@ const projects = [{
   id: 1,
   name: "one",
   updatedAt: 'Sept 2, 1990'
+},
+{
+  id: 2,
+  name: "two",
+  updatedAt: 'Feb 14, 1995'
 }];
 
 let testComponent;
@@ -26,7 +31,7 @@ before(function(done) {
 });
 
 describe('JSDom', () => {
-  it('loads window, document, and jQuery', () => {
+  it('should render window, document, and jQuery', () => {
     expect(window).to.be.an('object');
     expect(document).to.be.an('object');
     expect($).to.be.a('function');
@@ -34,14 +39,16 @@ describe('JSDom', () => {
 });
 
 describe('Projects component', () => {
-  it('renders correctly', () => {
-
+  it(`should render 'project' div correctly`, () => {
     let testNode = ReactDOM.findDOMNode(testComponent);
 
     expect(testNode.nodeName).to.be.equal('DIV');
     expect(testNode.className).to.be.equal('projects');
-    expect(testNode.getElementsByTagName('LI').length).to.be.equal(1);
     expect(testNode.getElementsByTagName('LI')[0]).to.be.an('object');
     expect(testNode.getElementsByTagName('SPAN')[0].textContent).to.be.equal(`Title: ${projects[0].name} Updated At: ${projects[0].updatedAt}`);
+  });
+  it('should render list of all projects in db', () => {
+    let testNode = ReactDOM.findDOMNode(testComponent);
+    expect(testNode.getElementsByTagName('LI').length).to.be.equal(2);
   });
 });
