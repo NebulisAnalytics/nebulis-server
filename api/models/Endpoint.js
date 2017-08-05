b919faa6d94dc2e852feed1288d50489fdffdcf2
Christopher Dobler <chrisdobler@gmail.com>
2017-08-04 22:35:56 -0700

    Adds model callback to add repo for endpoint

--- api/models/Endpoint.js
+++ api/models/Endpoint.js
@@ -4,6 +4,8 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
+var rimraf = require('rimraf');
+const request = require('request');
 
 module.exports = {
 
@@ -17,5 +19,28 @@ module.exports = {
     team: {
       model: 'team',
     },
+  },
+
+//lifecycle callbacks
+
+  afterCreate: function(newRecord, cb) {
+    let res;
+    request.post(`http://localhost:7010/reset`, (err, httpResponse, body) => {
+      if (err) { return cb(err); }
+      cb();
+    });
+  },
+
+  //TODO: should destroy endpoint
+  afterDestroy: function(destroyedRecords, cb) {
+    let count = 0;
+    // destroyedRecords.forEach((record) => {
+    //   console.log('deleting repo:' + record.id);
+    //   rimraf('/tmp/repos/' + record.id + '.git', () => {
+    //     count += 1;
+    //   });
+    //   if(count === destroyedRecords.length) cb();
+    // });
+    cb();
   },
 };
