f845a10a5b6b5960933ffef73000b1e8f995d82a
Christopher Dobler <chrisdobler@gmail.com>
2017-08-05 09:52:28 -0700

    creates repo dir on test start

--- test/server/config.js
+++ test/server/config.js
@@ -1,12 +1,15 @@
 const sails = require('sails');
 const nebugit = require('../../gitnet');
+const fs = require('fs');
 
 before(function(done) {
-  this.timeout(20000);	
-  nebugit.listen();	
-  sails.lift(function() {
-    done();
-  });
+  this.timeout(20000);
+  fs.mkdir('/tmp/repos', function (err) {
+    nebugit.listen();	
+    sails.lift(function() {
+      done();
+    });
+});	
 });
 
 after((done) => {
