59097a9cbc594965ce896c14e75ff864cc92e2c8
OnuPete <pcr137@case.edu>
2017-08-05 19:10:51 -0700

    added Login view

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -3,10 +3,5 @@ import nebulisActions from './../actions/nebulisActions';
 
 export default class Login extends Component {
 	render() {
-		return (<div className="loginButton"><a href="/github"><button>Login with GitHub!</button></a></div>);
 	}
-
-  loginFunc() {
-    nebulisActions.login(null);
-  }
 }
