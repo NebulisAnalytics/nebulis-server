d1c4115adc648ee6d8867147ba6381c95c2934b6
Christopher Dobler <chrisdobler@gmail.com>
2017-08-06 00:02:24 -0700

    Updates to integration tests

--- test/endpoint/tests/EndpointIntegrationTest.js
+++ test/endpoint/tests/EndpointIntegrationTest.js
@@ -11,44 +11,52 @@ const expect = chai.expect;
 let connector;
 
 before((done) => {
-  connector = spawn( 'npm', [ 'install' ], {
+  connector = spawn( 'yarn', {
     cwd: './test/endpoint/testingProject/',
   });
 
   connector.stdout.on('data', (message) => {
     console.log(message.toString());
-    done();
+    if (message.toString().indexOf('Done in') !== -1) {
+      done();
+    }
   });
 });
 
 describe('Endpoint Application Integration', function() {
+  let project;
+  let endpointID;
   before((done) => {
     this.timeout(20000);
-
+    spawnSync( 'mkdir', [ './.git' ], {
+      cwd: './test/endpoint/testingProject',
+    });
     spawnSync( 'cp', [ 'sampleconfig', './testingProject/.git/config' ], {
-      cwd: './test/endpoint/',
+      cwd: './test/endpoint',
     });
-
-    connector = spawn( 'node', [ 'index.js' ], {
-      cwd: './test/endpoint/testingProject/',
+    Project.create({name: 'coding-challenge-1', gitLink: 'github.com/user/testProj', slug: 'coding-challenge-1'}).exec((err, res) => {
+      project = res;
+      connector = spawn( 'node', [ 'index.js' ], {
+        cwd: './test/endpoint/testingProject/',
+      });
+      connector.stdout.on('data', beforeListener);
+      connector.stderr.on('data', beforeListener);
     });
-
-    connector.stdout.on('data', (message) => {
+    const beforeListener = (message) => {
       console.log(message.toString());
-    });
-
-    setTimeout(() => {
-      done();
-    }, 8000);
-
+      if (message.toString().indexOf('Endpoint ID:') !== -1) {
+        const m = message.toString();
+        const i = m.indexOf('ID:');
+        endpointID = m.substring(i + 4, m.length).split(' ')[0].split('\n')[0];
+      }
+      if (message.toString().indexOf('Checking for changes...') !== -1) {
+        setTimeout(() => {
+          connector.stdout.removeListener('data', beforeListener);
+          done();
+        }, 1000);
+      }
+    }
     // connector.on('close', (code, signal) => { done(); });
-
-    //install npm modules
-
-    //create git config file
-
-    //launch process
-
   });
   after((done) => {
     spawnSync( 'rm', [ './newfile.js' ], {
@@ -57,26 +65,46 @@ describe('Endpoint Application Integration', function() {
     spawnSync( 'rm', [ './testingProject/.git/config' ], {
       cwd: './test/endpoint/',
     });
-  
+    spawnSync( 'rm', [ '-rf', '.git' ], {
+      cwd: './test/endpoint/testingProject',
+    });
     spawnSync( 'rm', [ '-rf', './testingProject/.nebugit' ], {
       cwd: './test/endpoint/',
     });
-    done();
+    Project.destroy(project.id).exec((err) => {
+      Endpoint.find(endpointID).populate('member').exec((err, endpoints) => {
+        Endpoint.destroy(endpointID).exec((err) => {
+          Member.destroy(endpoints[0].member.id).exec((err) => {
+          done();
+          });
+        });
+      });
+    });
   });
   xit('should be able to connect to the server', () => {
 
   });
-
   it('on file change change, it should make a successful push to a git repo', (done) => {
-
-    connector.stdout.on('data', (message) => {
+    let madeCommit = false;
+    const pushListener = (message) => {
+      if (message.toString().indexOf('1 file changed, 0 insertions(+)') !== -1) {
+        madeCommit = true;
+      };
+      if (message.toString().indexOf('Syncing endpoint to server...') !== -1) {
+        expect(madeCommit).to.be.equal(true);
+        setTimeout(() => {
+          connector.stdout.removeListener('data', pushListener);
+          done();
+        }, 750);
+      };
       console.log(message.toString());
-      done();
-    });
-
+    };
+    connector.stdout.on('data', pushListener);
     spawnSync( 'touch', [ './newfile.js' ], {
       cwd: './test/endpoint/testingProject',
     });
+  });
+  xit('should report an additional commit in the server repo', (done) => {
 
   });
 });
