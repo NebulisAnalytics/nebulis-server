fb03fe6c2eba4ff38f2208dbb11913ef790e70e5
Christopher Dobler <chrisdobler@gmail.com>
2017-08-07 14:54:55 -0700

    Extracts routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
-
-import Home from './routes/Home';
-import Login from './routes/Login';
-import ProjectPage from './routes/projects/index';
-import ProjectContainer from './routes/projects/view';
-import MembersPage from './routes/MembersPage';
-import Page2 from './routes/Page2';
-import NotFound from './routes/NotFound';
+import routes from './routes';
+import { Route, Router, IndexRoute, browserHistory } from 'react-router';
 
 export default class App extends Component {
   constructor(props) {
@@ -16,14 +9,8 @@ export default class App extends Component {
   render() {
     return (
       <Router history={browserHistory}>
-        <Route path="/">
-          <IndexRoute component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/projects" component={ProjectPage} />
-          <Route path="/projects/:id" component={ProjectContainer} />
-          <Route path="/members" component={MembersPage} />
-          <Route path="*" component={NotFound} />
-        </Route>
-      </Router>);
+        {routes}
+      </Router>
+    );
   }
 }
