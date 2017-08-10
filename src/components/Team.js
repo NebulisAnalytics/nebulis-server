268bc67fbf0529d3944d56c7f994b4d1c6688263
OnuPete <pcr137@case.edu>
2017-08-09 22:48:09 -0700

    moved download icon into list item

--- src/components/Team.js
+++ src/components/Team.js
@@ -4,31 +4,35 @@ import FloatingActionButton from 'material-ui/FloatingActionButton';
 import { DownloadIcon } from 'mdi-material-ui';
 
 export default function Team ({team, onTeamOpen, onDownload}) {
+  const downloadButton = (
+    <FloatingActionButton style={styles.button} secondary={true} onTouchTap={onDownload} mini={true}>
+      <DownloadIcon />
+    </FloatingActionButton>
+  )
   return (
     <div style={styles.container}>
       <ListItem
         style={styles.listItem}
+        primaryText= { "Updated At: " + team.updatedAt }
         secondaryText= { "Updated At: " + team.updatedAt }
         hoverColor= { '#FF8442' }
         onTouchTap={onTeamOpen}
+        rightIconButton={downloadButton}
         />
-      <FloatingActionButton secondary={true} onTouchTap={onDownload} mini={true}>
-        <DownloadIcon />
-      </FloatingActionButton>
+
     </div>
   )
 }
 
+
+
 const styles = {
   container: {
-
   },
   button: {
-    width: '20%',
-    display: 'block'
+    marginRight: '20px'
   },
   listItem: {
-    width: '78%',
-    display: 'block'
+    textAlign: 'center',
   }
 }
