4c78751bc0c9f58eb9c7a8ee5ed7c0dd1e5eb8e1
Christopher Dobler <chrisdobler@gmail.com>
2017-08-18 12:14:24 -0700

    adds ssl to postgres

--- config/connections.js
+++ config/connections.js
@@ -89,6 +89,7 @@ module.exports.connections = {
   productionPostgresqlServer: {
     adapter: 'sails-postgresql',
     host: process.env['DATABASE_URL'],
+    ssl: true
   }
 
   /***************************************************************************
