efe303043100712ae00ebb14a10e4e7549eda428
Christopher Dobler <chrisdobler@gmail.com>
2017-08-18 20:43:49 -0700

    Fixes to endpoint download

--- api/controllers/EndpointController.js
+++ api/controllers/EndpointController.js
@@ -110,7 +110,7 @@ module.exports = {
         console.log(members);
         if (members[0].endpoints.length > 0) {
           const eid = members[0].endpoints[0].id;
-          const path = `${process.env['REPO_LOCATION']}/repos/${eid}.git`;
+          const path = `${process.env['REPO_LOCATION']}/${eid}.git`;
           console.log(path);
           git.Clone.clone(path, `${store}/browse`).then(function(repository) {
             console.log('repo',repository)
